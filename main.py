5f98a938ddfcbe11c5063fbd6bf5603530a11cfd
hazre <37149950+hazre@users.noreply.github.com>
2025-03-07 16:11:42 +0100

    refactor to use environment variables and add delete functionality
    - Added .env.example file for storing credentials
    - Implemented dotenv for loading environment variables
    - Created new delete_all_loved_songs function to remove LastFM loved tracks
    - Restructured code with proper function organization
    - Improved logging and user feedback messages
    - Changed behavior to skip already loved songs instead of stopping

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ MIT License
 
 Copyright (c) 2024 Raphael Maschinsen
 raphaelmaschinsen@gmail.com
+Copyright (c) 2025 hazre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -27,21 +28,29 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pylast
 import os
+import argparse
 from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Transfer Spotify liked songs to LastFM or delete all loved songs from LastFM')
+    parser.add_argument('--deleteAll', action='store_true', help='Delete all loved songs from LastFM account')
+    return parser.parse_args()
 
 # Limit how many liked songs you want to transfer (order is starting with the most recent likes)
 # Set to None to transfer all liked songs
 LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER: Optional[int] = None
-LOG_FILE = 'loved_songs.log'
 
-# Replace these with your actual credentials
-SPOTIFY_CLIENT_ID = 'SPOTIFY_CLIENT_ID'
-SPOTIFY_CLIENT_SECRET = 'SPOTIFY_CLIENT_SECRET'
-SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'
-LASTFM_API_KEY = 'LASTFM_API_KEY'
-LASTFM_API_SECRET = 'LASTFM_API_SECRET'
-LASTFM_USERNAME = 'LASTFM_USERNAME'
-LASTFM_PASSWORD = 'LASTFM_PASSWORD'
+LOG_FILE = os.getenv('LOG_FILE') or 'loved_songs.log'
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI') or 'http://localhost:8888/callback'
+LASTFM_API_KEY = os.getenv('LASTFM_API_KEY')
+LASTFM_API_SECRET = os.getenv('LASTFM_API_SECRET')
+LASTFM_USERNAME = os.getenv('LASTFM_USERNAME')
+LASTFM_PASSWORD = os.getenv('LASTFM_PASSWORD')
 
 # Step 1: Authenticate with Spotify
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
@@ -72,59 +81,93 @@ def log_loved_song(file_path: str, artist: str, title: str, id_: str):
 # Load already loved song identifiers from the log file
 loved_songs_log = read_loved_songs_log(LOG_FILE)
 
-# Initialize variables for loop
-offset = 0
-liked_songs_count = 0
-encountered_already_loved_song = False
+def delete_all_loved_songs(network: pylast.LastFMNetwork):
+    """Delete all loved songs from LastFM account"""
+    print("Starting to delete all loved songs from LastFM...")
+    deleted_count = 0
+    batch_size = 50
 
-while True:
-    # Fetch liked songs in batches of 50
-    results = sp.current_user_saved_tracks(limit=50, offset=offset)
-    liked_songs = results['items']
+    while True:
+        # Get a batch of loved tracks
+        loved_tracks = network.get_user(LASTFM_USERNAME).get_loved_tracks(limit=batch_size, cacheable=True)
+        if not loved_tracks:
+            break
+            
+        for loved_track in loved_tracks:
+            track = loved_track.track
+            print(f"Unlove track: {track.artist.name} - {track.title}")
+            track.unlove()
+            deleted_count += 1
+        
+        # If we got fewer tracks than the batch size, we've reached the end
+        if len(loved_tracks) < batch_size:
+            break
+    
+    print(f"Successfully deleted {deleted_count} loved songs from LastFM!")
 
-    if not liked_songs:  # If no more songs are found, break out of the loop
-        break
+def transfer_liked_songs():
+    """Transfer Spotify liked songs to LastFM"""
+    # Initialize variables for loop
+    offset = 0
+    liked_songs_count = 0
+    skipped_songs_count = 0
 
-    for item in liked_songs:
+    while True:
+        # Fetch liked songs in batches of 50
+        results = sp.current_user_saved_tracks(limit=50, offset=offset)
+        liked_songs = results['items']
+
+        if not liked_songs:  # If no more songs are found, break out of the loop
+            break
+
+        for item in liked_songs:
+            if (
+                LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER is not None and
+                liked_songs_count >= LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER
+            ):
+                break
+
+            track = item['track']
+            track_id = track['id']  # Get the unique track ID
+            artist_name = track['artists'][0]['name']
+            track_name = track['name']
+            song_identifier = f"{artist_name} - {track_name} - {track_id}"
+
+            # Skip if the song is already loved
+            if song_identifier in loved_songs_log:
+                print(f"Skipping already loved song: {track_name} by {artist_name}")
+                skipped_songs_count += 1
+                continue
+
+            print(f"Loving {track_name} by {artist_name} on Last.fm")
+
+            # Love the song on Last.fm
+            track_lfm = network.get_track(artist_name, track_name)
+            track_lfm.love()
+
+            # Log the song details to the file
+            log_loved_song(LOG_FILE, artist_name, track_name, track_id)
+
+            liked_songs_count += 1
+
         if (
             LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER is not None and
             liked_songs_count >= LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER
         ):
             break
 
-        track = item['track']
-        track_id = track['id']  # Get the unique track ID
-        artist_name = track['artists'][0]['name']
-        track_name = track['name']
-        song_identifier = f"{artist_name} - {track_name} - {track_id}"
+        # Increase offset to get the next batch of liked songs
+        offset += 50
 
-        # Stop the process if the song is already loved
-        if song_identifier in loved_songs_log:
-            print(f"Encountered already loved song: {track_name} by {artist_name}. Stopping the process.")
-            encountered_already_loved_song = True
-            break
+    print(f"Done! Loved {liked_songs_count} new songs and skipped {skipped_songs_count} already loved songs on Last.fm!")
 
-        print(f"Loving {track_name} by {artist_name} on Last.fm")
+def main():
+    args = parse_args()
+    
+    if args.deleteAll:
+        delete_all_loved_songs(network)
+    else:
+        transfer_liked_songs()
 
-        # Love the song on Last.fm
-        track_lfm = network.get_track(artist_name, track_name)
-        track_lfm.love()
-
-        # Log the song details to the file
-        log_loved_song(LOG_FILE, artist_name, track_name, track_id)
-
-        liked_songs_count += 1
-
-    if encountered_already_loved_song or (
-        LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER is not None and
-        liked_songs_count >= LIMIT_NUMBER_OF_RECENT_LIKES_TO_TRANSFER
-    ):
-        break
-
-    # Increase offset to get the next batch of liked songs
-    offset += 50
-
-if encountered_already_loved_song:
-    print("Stopped because it encountered an already logged liked song.")
-else:
-    print(f"Done loving {liked_songs_count} liked songs on Last.fm!")
+if __name__ == "__main__":
+    main()
